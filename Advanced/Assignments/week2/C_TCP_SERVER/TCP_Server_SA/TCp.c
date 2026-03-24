bf42cb935e57010b98649146e08d00f6ba12972d
khushal4408x <yadavkhushal25@gmail.com>
2026-03-24 19:41:42 +0530

    rough

--- Advanced/Assignments/week2/C_TCP_SERVER/TCP_Server_SA/TCp.c
+++ Advanced/Assignments/week2/C_TCP_SERVER/TCP_Server_SA/TCp.c
@@ -1,6 +1,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include<arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
 
---
+++ Advanced/week4/backendForSum.js
@@ -0,0 +1,12 @@
+const express=require ('express')
+const cors=require('cors')
+
+const port=3000;
+const app=express();
+app.use(cors());
+app.get('/',function(req,res){
+    const a=req.query.a;
+    const b=req.query.b;
+    res.send(parseInt(a)+parseInt(b));
+})
+app.listen(port)
